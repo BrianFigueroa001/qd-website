d6a97c9984916eea3d61d8b9bb65e5956d8c8ece
BrianFigueroa001 <brianfigueroa001@gmail.com>
2020-12-29 18:57:38 -0500

    Added basic functionality to detect and/or create art directory

--- app.py
+++ app.py
@@ -1,10 +1,22 @@
+import os
 from flask import Flask, render_template
+from __future__ import print_function
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():
+def open_website():
+    ART_DIR = os.getcwd() + "/qd-art"
+
+    if not os.path.isdir(ART_DIR):
+        print("Art directory not found.")
+        print("Creating directory.")
+        os.mkdir(ART_DIR)
+
+    print("Moving to art directory.")
+    os.chdir(ART_DIR)
+
     return render_template('index.html')
 
 
